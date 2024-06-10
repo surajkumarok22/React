8ded796ca26a3f2ce4c710cd49dd9b204c40d385
Suraj Kumar <surajkumarok20@gmail.com>
2024-06-10 19:42:02 +0530

    error

--- LotteryReact/LotteryGame/src/LotteryGame.jsx
+++ LotteryReact/LotteryGame/src/LotteryGame.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import "./Lottery.css"
 import { genTicket, sum } from "./helper.js";
+import Ticket from "./Ticket.jsx"
 
 
 export default function LotteryGame({ n =3 , WinningSum = 15}){
@@ -16,7 +17,7 @@ let buyTicket = () => {
     return (
         <div>
             <h1>Lottery Game</h1>
-           <h1><Ticket  ticket = {ticket}/></h1>
+            <Ticket  ticket = {ticket}/>
             <button onClick={buyTicket}>Buy New Ticket</button>
 
             <h3>{isWinning && "Congratulation, you won!"}</h3>
